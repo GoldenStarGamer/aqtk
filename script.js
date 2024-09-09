5c7de9d202a0d263fc79457cd78b22fda9e19b6d
Starman <vicentetavareslima@gmail.com>
2024-09-09 22:02:23 +0100

    added button to youtube playlist

--- script.js
+++ script.js
@@ -59,3 +59,7 @@ document.getElementById("discord").addEventListener("click", function() {
 document.getElementById("code").addEventListener("click", function() {
 	window.location.href = "https://gitlab.com/grstarman/ProjectBadBot";
 });
+
+document.getElementById("fdev").addEventListener("click" , function() {
+	window.location.href = "https://www.youtube.com/playlist?list=PLL4cl01Ww8_pj3QI4aqV0TLKQYX0J459J"
+})
