463845f2fd4c91b4fb16459d472b7dd7020c4730
Starman <vicentetavareslima@gmail.com>
2025-04-04 13:59:00 +0100

    fixed links

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ if (!firstImpression()) {
 	document.getElementById("welcome").textContent="Welcome Back";
 }
 
-function linkbtn(id:String, link:String) {
-  document.getElementById(id).addEventListener("click", () => window.location.href=link
+function linkbtn(id, link) {
+  document.getElementById(id).addEventListener("click", () => window.location.href=link)
+}
 
 linkbtn("discord", "https://discord.gg/QTUURzPqYa")
 linkbtn("peth", "peth/")
