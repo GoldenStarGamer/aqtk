c5f0f93647ac30d8354187a83c2f262615850392
Starman <vicentetavareslima@gmail.com>
2025-04-08 18:43:07 +0100

    add the playlist link

--- peth/script.js
+++ peth/script.js
@@ -1,3 +1,3 @@
 document.getElementById("fdev").addEventListener("click" , function() {
-	window.location.href = "https://www.youtube.com/@GRStarman"
+	window.location.href = "https://youtube.com/playlist?list=PLL4cl01Ww8_qR1_DEmX1lcpf1AbZ9dH8D"
 })
