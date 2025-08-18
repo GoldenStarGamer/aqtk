53d9d4e84a01957a9214bc651435e97213ff6646
StarmanAkremis <vicentetavareslima@gmail.com>
2025-08-18 02:07:58 +0100

    update

--- script.js
+++ script.js
@@ -6,5 +6,9 @@ function linkbtn(id, link) {
   document.getElementById(id).addEventListener("click", () => window.location.href=link)
 }
 
-linkbtn("discord", "https://discord.gg/pMRmc2uF64")
+linkbtn("discord", "invite/")
 linkbtn("peth", "peth/")
+
+linkbtn("nets", "https://gitlab.com/arqitek/NetShaderc")
+linkbtn("alk", "https://gitlab.com/arqitek/Alkata")
+linkbtn("arq", "https://gitlab.com/arqitek/Arqeta")
